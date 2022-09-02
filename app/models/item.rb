bfb3c70215647bad4ef56350854cd8fc98454dbf
Alex Boyd <alexanderleeboyd@gmail.com>
2022-09-01 23:20:26 -0400

    Fix: model test coverage

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,6 +2,8 @@ class Item < ApplicationRecord
   extend Searchable
 
   belongs_to :merchant
+  has_many :invoices, through: :merchant
+  has_many :invoice_items, through: :invoices
 
   validates :name, presence: true
   validates :description, presence: true
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,6 @@ class Merchant < ApplicationRecord
   extend Searchable
 
   has_many :items
+  has_many :invoice_items, through: :items
+  has_many :invoices
 end
