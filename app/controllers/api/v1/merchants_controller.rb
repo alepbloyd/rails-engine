fc90aec602cce12542860258f768c69f2a3daaba
Alex Boyd <alexanderleeboyd@gmail.com>
2022-09-01 21:37:43 -0400

    Refactor: create MerchantFinder poro

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,25 +1,41 @@
 class Api::V1::MerchantsController < ApplicationController
+  before_action :access_merchant, only: [:show]
+  before_action :access_merchants, only: [:index]
+  before_action :make_finder, only: [:find, :find_all]
 
   def index
-    merchant_json_response(Merchant.all)
+    merchant_json_response(@merchants)
   end
 
   def show
-    merchant = Merchant.find(params[:id])
-    merchant_json_response(merchant)
+    merchant_json_response(@merchant)
   end
 
   def find
-    merchant = Merchant.find_one_by_name(params[:name])
+    results = @merchant_finder.search_one
     
-    if merchant.nil?
+    if results.nil?
       render json: { data: {} }, status: 200
     else
-      merchant_json_response(merchant)
+      merchant_json_response(results)
     end
   end
 
   def find_all
+  end
+
+  private
+
+  def access_merchant
+    @merchant = Merchant.find(params[:id])
+  end
+
+  def access_merchants
+    @merchants = Merchant.all
+  end
+
+  def make_finder
+    @merchant_finder = MerchantFinder.new(params[:name])
   end
 
 end
